52600d20ad56f04a130a0cba49ba989db598cb55
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-08-20 11:05:04 -0400

    makeRunOn

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+const makeRunOn = function(str){
+  // new given str
+  let result = '';
+// //loop through the given string
+for(let i = 0; i < str.length; i++){
+//if the string ends with a period, replace it with a comma
+  if (str[i]==="." && i!== str.length-1){
+    result = result + ","
+  }//otherwise leave it alone (the last period)
+  else {
+    result = result + str[i]
+  }
+}
+return result
+}
 
+// const totalScore = function(score){
+//   return score + score
 
-
+// }
 
 
 
